408cf159b07ea44c7373aebe3feadcdecc23109c
Lawar James Chaudhary <140185232+lawarjameschaudhary@users.noreply.github.com>
2025-01-14 18:38:29 +0545

    initial change

---
+++ script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  
